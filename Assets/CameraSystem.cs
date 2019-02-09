ac16315e0cccaedaba928275f466a36b318757e1
qbielka@sfu.ca <qbielka@sfu.ca>
2019-02-09 13:10:40 -0800

    fixed broken build

--- Assets/CameraSystem.cs
+++ Assets/CameraSystem.cs
@@ -12,7 +12,7 @@ public class CameraSystem : MonoBehaviour
 
     // Start is called before the first frame update
     void Start() {
-        player = GetObject.FindGameObjectWithTag ("player");
+
     }
 
     // Update is called once per frame
