58c08fd5cbccc0debc52b66976c5cba5aa45db75
qbielka@sfu.ca <qbielka@sfu.ca>
2019-02-09 11:29:39 -0800

    Raptor

---
+++ Assets/RaptorMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaptorMove : MonoBehaviour {
+
+    int moveSpeed = 15;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+	}
+}
