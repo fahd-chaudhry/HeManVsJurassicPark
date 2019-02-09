9f2935d526632752865179d71e0d039351fe7e13
Karan Nedungadi <755980@gmail.com>
2019-02-09 14:46:13 -0800

    heman_life update

--- Assets/heman_life.cs
+++ Assets/heman_life.cs
@@ -1,32 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class heman_life : MonoBehaviour
 {
     public int health;
-    public bool hasDied;
+    public bool isAlive;
     // Start is called before the first frame update
     void Start()
     {
-        hadDied = false;
+        hadDied = true;
     }
 
     void Update()
     {
-        if(gameObject.transform.position.y < -7)
+        if (gameObject.transform.position.y < -7)
         {
-            hasDied = true;
-        }
-        if(hasDied == true)
-        {
-
+            Die();
         }
     }
 
-    IEnumerator Die()
+    void Die()
     {
-        yeild return new WaitForSeconds(2);
-        Debug.log("Player has died");
+        SceneManager.LoadScene("Prototype_1"); //restart level
+    }   
+
+    void onCollisionEnter2D (Collision2D call)
+    {
+        if(CollectionBase.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
     }
 }
