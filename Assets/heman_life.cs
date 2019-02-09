ee567e79161dcf7477f6b3ff4eac4b407a93e71e
Karan Nedungadi <755980@gmail.com>
2019-02-09 13:54:36 -0800

    heman_life update

---
+++ Assets/heman_life.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heman_life : MonoBehaviour
+{
+    public int health;
+    public bool hasDied;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hadDied = false;
+    }
+
+    void Update()
+    {
+        if(gameObject.transform.position.y < -7)
+        {
+            hasDied = true;
+        }
+        if(hasDied == true)
+        {
+
+        }
+    }
+
+    IEnumerator Die()
+    {
+        yeild return new WaitForSeconds(2);
+        Debug.log("Player has died");
+    }
+}
