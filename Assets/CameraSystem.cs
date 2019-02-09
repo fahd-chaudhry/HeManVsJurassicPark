dac634c3ae7c92b70cb899e17cb44fd1a4542aae
Fahd Chaudhry <fahd.chaudhry@yahoo.com>
2019-02-09 15:01:43 -0800

    Player can't continuously jump anymore.

--- Assets/CameraSystem.cs
+++ Assets/CameraSystem.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraSystem : MonoBehaviour
-{
+public class CameraSystem : MonoBehaviour {
+    
     private GameObject player;
     public float xMin;
     public float xMax;
@@ -16,7 +16,9 @@ public class CameraSystem : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() {
-        
+    void LateUpdate() {
+        float x = Mathf.Clamp (player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp (player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3 (x, y, gameObject.transform.position.z);
     }
 }
