eecb634b0458d9fc9879d84248be1f7f4a307180
Fahd Chaudhry <fahd.chaudhry@yahoo.com>
2019-02-09 16:19:05 -0800

    Added new player sprite and animations for idle and running states

--- Assets/heman_life.cs
+++ Assets/heman_life.cs
@@ -1,36 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
 
-public class heman_life : MonoBehaviour
-{
-    public int health;
-    public bool isAlive;
-    // Start is called before the first frame update
-    void Start()
-    {
-        hadDied = true;
-    }
+// public class heman_life : MonoBehaviour
+// {
+//     public int health;
+//     public bool isAlive;
 
-    void Update()
-    {
-        if (gameObject.transform.position.y < -7)
-        {
-            Die();
-        }
-    }
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         isAlive = true;
+//     }
 
-    void Die()
-    {
-        SceneManager.LoadScene("Prototype_1"); //restart level
-    }   
+//     void Update()
+//     {
+//         if (gameObject.transform.position.y < -7)
+//         {
+//             Die();
+//         }
+//     }
 
-    void onCollisionEnter2D (Collision2D call)
-    {
-        if(CollectionBase.gameObject.tag == "ground")
-        {
-            isGrounded = true;
-        }
-    }
-}
+//     void Die()
+//     {
+//         SceneManager.LoadScene("Prototype_1"); //restart level
+//     }   
+
+//     void onCollisionEnter2D (Collision2D call)
+//     {
+//         if(CollectionBase.gameObject.tag == "ground")
+//         {
+//             isGrounded = true;
+//         }
+//     }
+// }
